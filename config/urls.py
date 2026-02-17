6b210aa18340bc15906ff27c6e1cb88f1321eb79
saichandramouli <137763037+saichandramouli@users.noreply.github.com>
2026-02-17 13:29:28 +0530

    Fix formatting of admin URL path

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),   # 👈 This line is important
+    path('admin/', admin.site.urls),  
     path('api/', include('healthcare.urls')),
 ]
